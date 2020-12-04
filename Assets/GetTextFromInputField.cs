8fd14b8bc6f71d38cc8ea11b55e16cc99a085caa
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-12-04 16:26:12 +0900

    First commit.

---
+++ Assets/GetTextFromInputField.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetTextFromInputField : MonoBehaviour
+{
+    public string globalPlayerName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        globalPlayerName = GlobalVariable.playerName;    
+    }
+
+    public void getTextAndSetToGlobal()
+    {
+        Debug.Log("getTextAndSetToGlobal");
+        Debug.Log(gameObject.GetComponent<InputField>().text);
+        GlobalVariable.playerName = gameObject.GetComponent<InputField>().text;
+    }
+}
---
+++ Assets/SetPlayerName.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetPlayerName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Text>().text = GlobalVariable.playerName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // gameObject.GetComponent<Text>().text = GlobalVariable.playerName;
+    }
+
+    public void setName()
+    {
+        Debug.Log("SetName");
+        gameObject.GetComponent<Text>().text = GlobalVariable.playerName;
+    }
+}
